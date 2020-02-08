9c3b7d0045cf97bf4ab4d5130e9276cf21dc78b6
敲敲 <834041562@qq.com>
2020-02-09 00:09:29 +0800

    基本功能差不多

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var jwt = require('jsonwebtoken')
+var jwt = require('jsonwebtoken');
+var query = require('./config/db')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,29 +26,27 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+let user = null
+
 
 // 模拟一个登陆的接口
-app.post('/login',function(req,res){
-    let userlist = {user:'qiaoqiao',password:'15819745146zyc'}
-    console.log(req.body.user)
-    console.log(req.body.password)
-    if(userlist.user == req.body.user && userlist.password == req.body.password){
-        // 登录成功获取用户名
-        let username = req.body.user
-        res.json({
-            // 进行加密的方法
-            // sing 参数一：加密的对象 参数二：加密的规则 参数三：对象
-            token:jwt.sign({username:username},'abcd',{
-                // 过期时间
-                expiresIn:"1h"
-            }),
-            username,
-            code:200
-        })
-    }
-    else{
-        res.json({data:'登录失败'})
-    }
+app.post('/login',async function(req,res){
+    user = await query('select * from userinfo where username = ?',req.body.user)
+    let userlist = JSON.parse(JSON.stringify(user))
+        if(userlist[0].username == req.body.user && userlist[0].password == req.body.password){
+            let username = req.body.user
+            res.json({
+                // 进行加密的方法
+                // sing 参数一：加密的对象 参数二：加密的规则 参数三：对象
+                token:jwt.sign({username:username},'abcd',{
+                    // 过期时间
+                    expiresIn:"1h"
+                }),
+                username,
+                code:200
+            })
+        }
+    
 })
 
 // 登录持久化验证接口 访问这个接口的时候 一定要访问token（前端页面每切换一次，就访问一下这个接口，问一下我有没有登录/登陆过期）
@@ -73,6 +72,8 @@ app.post('/validate',function(req,res){
       }
   })
 })
-
+app.post('/postart',function(req,res){
+    query(`INSERT INTO articleinfo ( username , article ) VALUES ( '${req.body.username}' , '${req.body.article}' )`)
+})
 
 module.exports = app;
